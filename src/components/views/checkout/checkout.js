84b90a44b22781c77de94b54678267ed63f8b98d
Chet Lynch <chetlynch@Chets-MBP.hsd1.ma.comcast.net>
2018-10-03 11:49:05 -0400

    view structure created, TODO: view functionality

---
+++ src/components/views/checkout/checkout.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './checkout.css';
+
+
+
+class Checkout extends Component {
+
+
+  render() {
+    return (
+      <div className="Checkout">
+      <Navbar />
+      </div>
+    );
+  }
+}
+
+export default Checkout;
---
+++ src/components/views/home/home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './home.css';
+
+
+
+class Home extends Component {
+
+
+  render() {
+    return (
+      <div className="Home">
+      <Navbar />
+      </div>
+    );
+  }
+}
+
+export default Home;
