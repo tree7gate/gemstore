a9c36801e5884b44501832a9bf9b52ba021ccd5a
Chet Lynch <chetlynch@Chets-MBP.hsd1.ma.comcast.net>
2018-10-03 10:59:40 -0400

    included State for App component, set up products file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>, document.getElementById('root'));
+
+// hot reloads pages to keep state (not refreshing it)
+
+if (module.hot) {
+  module.hot.accept();
+}
